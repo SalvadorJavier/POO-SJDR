4bcef502e4d041ee167fc13225c87f877d9f16a5
T-102 <T-102@PC266>
2018-02-22 19:41:36 -0600

    excepsiones

---
+++ POO-SJDR/src/excepsiones/Primera.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepsiones;
+
+/**
+ *
+ * @author T-102
+ */
+public class Primera {
+    public static void main(String[] args){
+        
+        int[] valores={2,6,-8,5};
+        
+        for(int i=0; i<=valores. length; i++){
+      //  System.out.println("valor"+valores[i]);
+        
+    }
+        //essta excepcion es muy trucukenta
+  int x=5;
+  int y=0;
+  //System.out.println(x/y); arritmetic exception
+      int z=Integer.parseInt("x");
+      System.out.println(x+z);
+    }
+    
+}
