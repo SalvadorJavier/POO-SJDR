167ee22ea0824db823a82ce1adbac2d90a05b8f5
Aranza <Aranza@Aranza-VAIO>
2018-04-19 15:37:36 -0500

    cuestionario

---
+++ POO-SJDR/src/cuestionario/ModeloCuestionario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuestionario;
+
+import java.util.*;
+public class ModeloCuestionario {
+    ArrayList<Pregunta> preguntas;
+    
+    public ModeloCuestionario(){
+        //Aqui vamos a generar nuestro cuetionario
+        preguntas=new ArrayList<>();
+        
+    }
+    public void agregarPregunta(Pregunta p){
+        preguntas.add(p);
+    }
+    
+    public ArrayList<Pregunta> obtenerCuestionario(){
+        return preguntas;
+    }
+    
+}
---
+++ POO-SJDR/src/cuestionario/Pregunta.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuestionario;
+
+import java.util.*;
+
+/**
+ *
+ * @author juan_
+ */
+public class Pregunta {
+    private String titulo;
+    private ArrayList<Opcion> opciones;
+
+    public Pregunta() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public ArrayList<Opcion> getOpciones() {
+        return opciones;
+    }
+
+    public void setOpciones(ArrayList<Opcion> opciones) {
+        this.opciones = opciones;
+    }
+    
+    
+}
